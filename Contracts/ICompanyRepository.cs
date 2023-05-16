d675c806ea25fcedc80169a590e0cefa86917c54
Prashant Giri <ext_prashant.giri@coats.com>
2023-05-16 23:46:24 +0530

    Add project files.

---
+++ Contracts/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+﻿using DapperTest.Entities;
+
+namespace DapperTest.Contracts
+{
+    public interface ICompanyRepository
+    {
+        public Task<IEnumerable<Company>> GetCompanies();
+        public Task<Company> GetCompany(int id);
+    }
+}
---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,48 @@
+﻿using DapperTest.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperTest.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepo;
+
+        public CompaniesController(ICompanyRepository companyRepo)
+        {
+            _companyRepo = companyRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            try
+            {
+                var companies = await _companyRepo.GetCompanies();
+                return Ok(companies);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}", Name = "CompanyById")]
+        public async Task<IActionResult> GetCompany (int id)
+        {
+            try
+            {
+                var company = await _companyRepo.GetCompany(id);
+                if(company == null)
+                    return NotFound();
+
+                return Ok(company);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
